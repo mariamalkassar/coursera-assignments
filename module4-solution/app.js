078e656613d567374faa8af5997606d257536b00
mariamalkassar <eng.mariamalkassar92@gmail.com>
2020-01-30 17:25:07 +0100

    tt

--- module4-solution/app.js
+++ module4-solution/app.js
@@ -3,7 +3,7 @@
 
     angular.module('ShoppingListDirectiveApp', [])
         .controller('ShoppingListController', ShoppingListController)
-        .service('MenuSearchService', MenuSearchService)
+        .service('ShoppingListservice', ShoppingListservice)
         .directive('shoppingList', ShoppingListDirective);
 
 
@@ -25,69 +25,48 @@
 
     function ShoppingListDirectiveController() {
         var list = this;
-
     }
 
 
-    ShoppingListController.$inject = ['MenuSearchService'];
+    ShoppingListController.$inject = ['ShoppingListservice'];
 
-    function ShoppingListController(MenuSearchService) {
-        var items = this;
-        items.searchTerm = '';
-        items.foundItems = [];
-        items.getMatchedMenuItems = function () {
-            items.foundItems = [
-                {name: 'mimi', short_name: "11", describtion: "tyryrty"},
-                {name: 'mimi', short_name: "11", describtion: "tyryrty"},
-                {name: 'mimi', short_name: "11", describtion: "tyryrty"},
-                {name: 'mimi', short_name: "11", describtion: "tyryrty"},
-            ]
-            // var promise = MenuSearchService.getMatchedMenuItems(items.searchTerm);
-            //
-            // promise.then(function (foundItems) {
-            //     items.foundItems = foundItems;
-            //     console.log("foundItems = ", items.foundItems);
-            // }).catch(function (error) {
-            //     console.log("Something went terribly wrong.");
-            // });
-        };
+    function ShoppingListController(ShoppingListservice) {
+        var list = this;
 
-        items.removeItem = function (itemIndex) {
-            alert("HHHHHHHHHHHHHHHHHHHHHHHi ");
-            items.foundItems.splice(itemIndex, 1);
+        list.founded = ShoppingListservice.getItems();
+
+        list.removeItem = function (itemIndex) {
+            alert("rrrrrrrrrrrrrrrrrr");
+            ShoppingListservice.removeItem(itemIndex);
         };
     }
 
 
 // If not specified, maxItems assumed unlimited
-    MenuSearchService.$inject = ['$http'];
+    ShoppingListservice.$inject = ['$http'];
 
-    function MenuSearchService($http) {
+    function ShoppingListservice($http) {
         var service = this;
-        var foundItems = [];
-        service.getMatchedMenuItems = function (searchTerm) {
-            return $http({
-                method: "GET",
-                url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-            }).then(function (result) {
-                var menu_items = result.data.menu_items;
+
+        // List of shopping items
+        var founded = [
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+            {name: 'mariam', short_name : 'AA', describtion: '21321'},
+        ];
 
 
-                for (var i = 0; i < menu_items.length; i++) {
-                    // console.log(menu_items[i].description);
-                    var description = menu_items[i].description;
-                    if (description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-                        foundItems.push(menu_items[i]);
-                    }
-                }
-                return foundItems;
-            });
-        };
+
         service.removeItem = function (itemIndex) {
-            foundItems.splice(itemIndex, 1);
+            founded.splice(itemIndex, 1);
         };
 
-
+        service.getItems = function () {
+            return founded;
+        };
     }
 
 })();
